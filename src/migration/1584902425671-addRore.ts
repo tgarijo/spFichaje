b26eec647633a57f4a15cac00ff5503a5559ca47
tgarijo <tgarijo@gmail.com>
2020-03-22 21:14:09 +0100

    Migrations

--- src/migration/1584902425671-addRore.ts
+++ src/migration/1584902425671-addRore.ts
@@ -1,11 +1,17 @@
+
 import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class addRore1584902425671 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
+          await queryRunner.query(`INSERT INTO "roles" (name) VALUES("ADMIN")`);
+          await queryRunner.query(`INSERT INTO "roles" (name) VALUES("USER")`);
+          await queryRunner.query(`INSERT INTO "roles" (name) VALUES("H.RESOURCES")`);
+          await queryRunner.query(`INSERT INTO "roles" (name) VALUES("MANAGER")`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`DELETE FROM "roles"`);
     }
 
 }
